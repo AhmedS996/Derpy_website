a7c4def134054e0dc941fbc3b592084f55a1e81e
Ahmed Alsaiqal <91352253+AhmedS996@users.noreply.github.com>
2024-04-26 20:17:24 +0300

    Create Api routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\AppController;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\GroupController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/events', [EventController::class, 'index']);
+Route::get('/groups', [GroupController::class, 'index']);
+Route::get('/users', [AppController::class, 'index']);
+
