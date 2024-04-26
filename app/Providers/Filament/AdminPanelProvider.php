7331f7ccadb0a9c586dd427e889d56433116a0d8
Ahmed Alsaiqal <91352253+AhmedS996@users.noreply.github.com>
2024-04-26 20:35:44 +0300

    customize panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -35,6 +35,9 @@ class AdminPanelProvider extends PanelProvider
                 'success' => Color::Emerald,
                 'warning' => Color::Orange,
             ])
+            ->font('Poppins')
+            ->brandName('Derby')
+            ->favicon(asset('images/derby.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
             ->pages([
