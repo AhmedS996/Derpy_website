ab9ad98b7c06be1b81a23de36942ef23828e3512
Ahmed Alsaiqal <91352253+AhmedS996@users.noreply.github.com>
2024-04-29 01:46:47 +0300

    Added FilamentServiceProvider
    and added Asset to use javascript

---
+++ app/Providers/FilamentServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
+
+class FilamentServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+    public function register(): void
+    {
+
+
+        FilamentAsset::register([
+            Js::make('RefreshData', __DIR__ . '/../../resources/js/RefreshData.js'),
+]);
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        //
+    }
+}
